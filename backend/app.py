ea62b7575640725721bd25e9b5f45479d796b488
LAKSHAY2100 <lakshaybajaj210@gmail.com>
2025-07-17 12:41:21 +0530

    final-changes

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
-from flask import Flask, request, render_template
+from flask import Flask, request
 import pickle
 from flask_cors import CORS
 import os
